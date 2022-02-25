ef7fdc03c39db5f033157d33897efb9d73d9b60c
DCastroAndrich <castroandrich.damian@gmail.com>
2022-02-25 15:49:28 -0300

    Funcionando al 100%

--- Desafios/Desafio08/tableSQLite.js
+++ Desafios/Desafio08/tableSQLite.js
@@ -5,8 +5,8 @@ knex.schema
   .createTable("messages", (table) => {
     table.increments("id").primary();
     table.string("author", 50).notNullable();
-    table.timestamp("created_at").defaultTo(knex.fn.now());
-    table.string("message", 300);
+    table.timestamp("date").defaultTo(knex.fn.now());
+    table.string("msg", 300);
   })
   .then(() => {
     console.log("Tabla creada!");
