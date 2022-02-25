8fb2b4bfcc2880016c4dc4d5c6d0ecb9b71bfc81
DCastroAndrich <castroandrich.damian@gmail.com>
2022-02-25 10:39:53 -0300

    Problemas con git, ya arreglado

---
+++ Desafios/Desafio08/server.js
@@ -0,0 +1,72 @@
+/* MODULOS */
+const express = require("express");
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+
+/* const ChatContainer = require("./containers/ChatContainer.js");
+const ProductsContainer = require("./containers/ProductsContainer.js"); */
+
+const Container = require("./src/containers/Container");
+const { optionsMySQL } = require("./src/utils/optionsMySQL");
+const { optionsSQLite } = require("./src/utils/optionsSQLite");
+const tableProducts = "products";
+const tableMessages = "messages";
+
+/* INSTANCIACION */
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+const apiProducts = new Container(optionsMySQL, tableProducts);
+const apiMessages = new Container(optionsSQLite, tableMessages);
+
+/* MIDDLEWARES */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+/* WEBSOCKET */
+io.on(
+  "connection",
+  /* async */ (socket) => {
+    console.log(`Nuevo cliente conectado ${socket.id}`);
+    //------- Enviar histórico de productos
+    socket.emit(
+      "products",
+      apiProducts.createTableMDB() && apiProducts.listAll()
+    );
+
+    //------- Escuchar nuevos productos
+    socket.on("newProduct", (product) => {
+      apiProducts.save(product);
+
+      //Actualización de la vista de productos
+      io.sockets.emit("products", apiProducts.listAll());
+    });
+
+    //------- Enviar histórico de mensajes
+    socket.emit(
+      "messages",
+      /* await */ apiMessages.createTableSQL() && apiMessages.getAll()
+    );
+
+    //------- Escuchar nuevos mensajes
+    socket.on(
+      "newMessage",
+      /* async */ (msg) => {
+        msg.date = new Date().toLocaleString();
+        /* await */ apiMessages.save(msg);
+
+        //Actualización de la vista de mensajes
+        io.sockets.emit("messages", /* await */ apiMessages.getAll());
+      }
+    );
+  }
+);
+
+/* SERVIDOR */
+const PORT = 8080;
+const server = httpServer.listen(PORT, () => {
+  console.log(`Servidor http escuchado en puerto ${server.address().port}`);
+});
+server.on("error", (error) => console.error(`Error en servidor ${error}`));
