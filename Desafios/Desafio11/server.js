fafb26733e5eb2d8191309278b1916a0b06b6e95
DCastroAndrich <castroandrich.damian@gmail.com>
2022-03-30 16:13:18 -0300

    Falta terminar desafio 11 - Errores con las vistas -

--- Desafios/Desafio11/server.js
+++ Desafios/Desafio11/server.js
@@ -1,12 +1,16 @@
 /* MODULOS */
-const ProductsMock = require("./src/mocks/ProductsMocks.js");
-
 const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
+const exhbs = require("express-handlebars");
+const path = require("path");
+
 
 const { Server: HttpServer } = require("http");
 const { Server: IOServer } = require("socket.io");
 
-
+const ProductsMock = require("./src/mocks/ProductsMocks.js");
 const Container = require("./src/containers/Container");
 const { optionsMySQL } = require("./src/utils/optionsMySQL");
 const { optionsSQLite } = require("./src/utils/optionsSQLite.js");
@@ -15,6 +19,14 @@ const tableProducts = "products";
 const tableMessages = "messages";
 
 const mockItems = new ProductsMock();
+
+const connectMongo = require("connect-mongo");
+
+const MongoStore = connectMongo.create({
+  mongoUrl:
+    "mongodb+srv://damian:damian@clusterex11.xtyhv.mongodb.net/Desafio11?retryWrites=true&w=majority",
+  ttl: 60,
+});
 
 /* INSTANCIACION */
 const app = express();
@@ -25,13 +37,69 @@ const apiProducts = new Container(optionsMySQL, tableProducts);
 const apiMessages = new Container(optionsSQLite, tableMessages);
 
 /* MIDDLEWARES */
+app.use(cookieParser())
+app.use(
+  session({
+    store: MongoStore,
+    secret: "123456789!#$%&/",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 60000}
+  })
+);
+
+app.set("views", path.join(__dirname, "./src/views"));
+
+app.engine(
+  "hbs",
+  exhbs.engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialDir: path.join(app.get("views"), "partials"),
+    extname: "hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 /* ROUTES */
 app.get("/api/productos-test", (req, res) => {
   res.json(mockItems.random());
+});
+
+/* pruebas */
+app.get("/", (req, res) => {
+  const nombre = req.session.nombre
+  console.log(nombre);
+  res.render('home', nombre);
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+
+app.post('/login', (req, res)=>{
+  const {nombre} = req.body
+  req.session.nombre = nombre
+
+  console.log(nombre);
+  res.redirect('/')
+})
+
+
+app.get("/logout", (req, res) => {
+  const {nombre} = req.body
+  req.session.nombre = nombre
+  
+  req.session.destroy((err) => {
+    if (!err) res.render("logout", nombre);
+    else res.send({ status: "Error al desloguear", body: err });
+  });
 });
 
 /* WEBSOCKET */
